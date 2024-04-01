00913bb6588a6a957c3ec4f2e291073ca41a69c1
iradukundapaci <paifique@gmail.com>
2024-04-01 21:21:50 +0200

    handling bugs

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python3
-"""
-Simple pagination helper function
+"""Pagination helper function.
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Function to create tuple pagination object
+    """Retrieves the index range from a given page and page size."""
 
-    args:
-        page: page number
-        page_size: size of elements
-
-    return: tuple of page and page size
-    """
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-"""Deletion-resilient hypermedia pagination
+"""Task 3: Deletion-resilient hypermedia pagination
 """
+
 import csv
-from typing import Dict, List
+import math
+from typing import Dict, List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size."""
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
 class Server:
@@ -11,9 +19,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a new Server instance."""
         self.__dataset = None
-        self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset"""
@@ -25,13 +31,15 @@ class Server:
 
         return self.__dataset
 
-    def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0"""
-        if self.__indexed_dataset is None:
-            dst = self.dataset()
-            truncated_dataset = dst[:1000]
-            self.__indexed_dataset = {i: dst[i] for i in range(len(dataset))}
-        return self.__indexed_dataset
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data."""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Retrieves info about a page from a given index and with a
